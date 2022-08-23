876408d964f0940e795c2b052d54e497796ad6f2
sgutwein <basti.gutwein@gmail.com>
2022-08-23 13:19:14 +0200

    Add argo-workflows cli package in version 3.3.9

---
+++ argo-workflows/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/argoproj/argo-workflows/releases/download/v3.3.9/argo-windows-amd64.exe.gz'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url64bit      = $url64
+  softwareName  = 'argo-workflows'
+  checksumType  = 'sha256'
+  checksum64    = 'ea21e993e6df60f9494b793a733505c3d0141bff2f957729ca1f75a610fbeded'
+  checksumType64= 'sha256'
+  silentArgs   = ''
+  validExitCodes= @(0)
+}
+Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum64 $checksum64 -checksumType64 $checksumType64
+Rename-Item $toolsDir/argo-windows-amd64.exe $toolsDir/$packageName.exe
