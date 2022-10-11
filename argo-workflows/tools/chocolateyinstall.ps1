f1660f4128995ef3c8ecb79860373a28c4adf145
sgutwein <basti.gutwein@gmail.com>
2022-10-11 16:45:35 +0200

    Update argo-workflows cli package to version 3.4.1

--- argo-workflows/tools/chocolateyinstall.ps1
+++ argo-workflows/tools/chocolateyinstall.ps1
@@ -1,16 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/argoproj/argo-workflows/releases/download/v3.4.0/argo-windows-amd64.exe.gz'
+$url64      = 'https://github.com/argoproj/argo-workflows/releases/download/v3.4.1/argo-windows-amd64.exe.gz'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url64bit      = $url64
   softwareName  = 'argo-workflows'
-  checksum64    = 'dcdcd8c619db0a46c80c3c5f83760d9e76141ba0e7926be3d30bb5df7131a981'
+  checksum64    = 'b65d795fb882643229f6862a82e5e7fc3324eeda24c77d01185c6a3c3c1b7753'
   checksumType64= 'sha256'
   silentArgs   = ''
   validExitCodes= @(0)
 }
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum64 $checksum64 -checksumType64 $checksumType64
-Rename-Item $toolsDir/argo-windows-amd64.exe $toolsDir/$packageName.exe
+Move-Item $toolsDir/argo-windows-amd64.exe $toolsDir/$packageName.exe
